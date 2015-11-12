5e779fd3c69eb01b306aa815958e63b5040025e1
Maile Lucks <maile.lucks@gmail.com>
2015-11-12 03:47:29 -0800

    Getting Logic Started

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
 
 $(document).ready(function(){
+
+	var DEBUG_MODE = true;
+		var debug = function(msg) {
+	    if (DEBUG_MODE == true) {
+	        console.log("DEBUG:", msg);
+	    }
+	}
 	
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
@@ -11,6 +18,33 @@ $(document).ready(function(){
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
+
+  	var guess = $("#userGuess").val();
+  	debug(guess);
+  	var mainNumber = Math.floor(Math.random()*100);
+  	debug(mainNumber);
+  	var guessDisplay = $('<li>' + guess + '</li>');
+
+  		function addGuesses(){
+
+	    $('.guessBox').prepend(guessDisplay);
+	    $('guess').val();
+
+  		};
+
+  		$('#guessButton').on('click', addGuesses);
+  		$('#userGuess').keydown(function(e) {
+        if (e.which == 13 ) {
+            addGuesses();
+        }
+  
+    	});
+    	// $('#userGuess').keydown(function(e) {
+	    //     if (e.which == 13 ) {
+	    //         addGuesses();
+	    //     }
+     //    });
+
 
 });
 
